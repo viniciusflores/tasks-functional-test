bf0d394dfd3363d5ff453b66ad094b3c954258c0
Vinicius Flores <viniciusflores379@gmail.com>
2020-08-05 00:21:35 +0000

    configure selenium grid

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -1,23 +1,27 @@
 package br.ce.wcaquino.tasks.functional;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
-	public WebDriver setupDriver() {
-		WebDriver driver = new ChromeDriver();
+	public WebDriver setupDriver() throws Exception {
+		// WebDriver driver = new ChromeDriver();
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://10.0.2.15:4444/wd/hub"), capabilities);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.navigate().to("http://localhost:8001/tasks/");
+		driver.navigate().to("http://10.0.2.15:8001/tasks/");
 		return driver;
 	}
 
 	@Test
-	public void shouldSaveTaskWithSuccess() {
+	public void shouldSaveTaskWithSuccess() throws Exception {
 		WebDriver driver = setupDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -30,9 +34,9 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
-	
+
 	@Test
-	public void shouldNotBeenPossibleToSaveTaskWithoutDescription() {
+	public void shouldNotBeenPossibleToSaveTaskWithoutDescription() throws Exception {
 		WebDriver driver = setupDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -44,9 +48,9 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
-	
+
 	@Test
-	public void shouldNotBeenPossibleToSaveTaskWithoutDate() {
+	public void shouldNotBeenPossibleToSaveTaskWithoutDate() throws Exception {
 		WebDriver driver = setupDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
@@ -59,9 +63,8 @@ public class TasksTest {
 		}
 	}
 
-
 	@Test
-	public void shouldNotBeenPossibleToSaveTaskWithPastDate() {
+	public void shouldNotBeenPossibleToSaveTaskWithPastDate() throws Exception {
 		WebDriver driver = setupDriver();
 		try {
 			driver.findElement(By.id("addTodo")).click();
